ca55deaac24e37a16ce0794f53df3a4cd8e238b0
eleahmloucks <eleahloucks@gmail.com>
2022-08-29 17:51:30 -0600

    finished part 2

--- objects.js
+++ objects.js
@@ -2,10 +2,29 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  let wordCounts = {};
+  let phraseArray = phrase.split(" ");
+  for (const word of phraseArray){
+    if (wordCounts.hasOwnProperty(word)){
+      wordCounts[word] += 1;
+    }else{
+      wordCounts[word] = 1;
+    }
+  }
+  return wordCounts;
 }
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
   // Replace this with your code
+  let melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+}
+  if (!melonPrices.hasOwnProperty(price)) {
+    return;
+  }
+  return melonPrices[price].sort();
 }
